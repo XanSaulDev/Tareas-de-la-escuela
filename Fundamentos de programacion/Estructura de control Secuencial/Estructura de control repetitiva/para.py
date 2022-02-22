4b407d327d0f5c5f0208bb97bb65c3366c495c0d
Huerta Murillo Alejandro Saul <saul901_@hotmail.com>
2022-02-21 19:34:08 -0600

    Cambiando encabezado de el archivo 'para.py'

--- Fundamentos de programacion/Estructura de control Secuencial/Estructura de control repetitiva/para.py
+++ Fundamentos de programacion/Estructura de control Secuencial/Estructura de control repetitiva/para.py
@@ -1,5 +1,5 @@
 #Python 3.8.5
-#Scrpit que muestra las 10 primeras potencias de un nummero
+#Scrpit que muestra de la letra "a" la letra "z"
 #Huerta Murillo Alejandro Saul 21/02/22
 
 import string
